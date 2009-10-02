a8c0d4e89db82a907bcef2f29fc501dd3a6534b8
Chris Mowforth <chris@mowforth.com>
2009-10-02 19:08:09 +0100

    Start testing the API code

--- lib/p4d.rb
+++ lib/p4d.rb
@@ -1,6 +1,6 @@
 $:.unshift File.dirname(__FILE__)
 %w(httparty cgi php_serialize ostruct).each { |external| require external }
-%w(string callable connection request).each { |f| require File.join(File.dirname(__FILE__), 'p4d', f) }
+%w(string callable connection request api).each { |f| require File.join(File.dirname(__FILE__), 'p4d', f) }
 
 module P4D
   VERSION = '0.0.1'
